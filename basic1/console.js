fecaa8f12775b1c0dbfabcf11da51eca494f397c
RAHUL KUMAR SAH <127338946+rsrahulsah@users.noreply.github.com>
2024-04-11 16:32:08 +0000

    discussion about variables and consoles

---
+++ basic1/console.js
@@ -0,0 +1,3 @@
+console.log("HC")
+console.error("HC")
+console.warn("HC")
---
+++ basic1/variables.js
@@ -0,0 +1,6 @@
+// variables -let
+//constants-const
+
+let userEmail ="rsrahulsah1432@gmail.com"
+const user_password="123"  //cosntant password we cannot change 
+console.log(user_password);
