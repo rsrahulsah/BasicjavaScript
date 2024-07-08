174f80dad60d9af4bf8b79b679fab5016c74318b
RAHUL KUMAR SAH <127338946+rsrahulsah@users.noreply.github.com>
2024-07-08 16:29:56 +0000

    this is all about datatypes

---
+++ basic1/datatypes.js
@@ -0,0 +1,50 @@
+// primitive(stack)
+/*
+
+string 
+number
+boolean
+Null
+undefined
+symbol
+BigInt
+
+
+*/
+const firstname="Rahul"
+const age=22
+const score=200.9
+let isLoggedIn=false
+const nodeversion=null
+let bankbalance=undefined
+const buttonId=symbol("id")
+const aReallyLondNumber=9974587546588
+
+
+
+//reference(non primitive)  -->objects(heap)
+
+/*
+
+Arrays 
+objects 
+function
+
+*/
+const numbers=[1,2,3,4,5,6]
+const heros=["superman","batman","rahul"]
+
+const userobject={
+    name:"rahul",
+    age:22,
+    isLoggedIn:true
+}
+
+//function
+const sayHello=function(){
+    console.log("rahul")
+}
+
+const registerUser=function(){}
+const loginuser=function(){}
+
