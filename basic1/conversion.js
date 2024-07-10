f8b7f376785903bf3161ab31e816ab1cc86581c4
RAHUL KUMAR SAH <127338946+rsrahulsah@users.noreply.github.com>
2024-07-10 16:34:25 +0000

    This is all about conversion

---
+++ basic1/conversion.js
@@ -0,0 +1,9 @@
+let bankBalance="100"
+
+let bankBalanceInInt=parseInt(bankBalance)
+
+console.log(bankBalance)
+console.log(bankBalanceInInt)
+
+console.log(typeof bankBalance)
+console.log(typeof bankBalanceInInt)
---
+++ basic1/numandstring.js
@@ -0,0 +1,5 @@
+const firstName='Rahul'
+const lastName='sah'
+
+console.log(firstName+' '+lastName)
+console.log(`My first name is ${firstName} and my last name is ${lastName}`)
