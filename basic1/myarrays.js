18f0630a01686dec16a138ca586624620d676dfe
RAHUL KUMAR SAH <127338946+rsrahulsah@users.noreply.github.com>
2024-07-15 15:39:55 +0000

    Discuss Arrays

---
+++ basic1/myarrays.js
@@ -0,0 +1,14 @@
+const arr=[4,5,7,8,4]
+console.log(arr)
+console.log(arr[1])
+
+//const arr1=new Array[1,2,3,4,5]
+//console.log(arr1[1])
+
+const dcHeros=["ramu","mohan","rahul","ramesh"]
+console.log(dcHeros)
+console.log(dcHeros.length)
+dcHeros.push("rah","mah")
+console.log(dcHeros)
+dcHeros.pop()
+console.log(dcHeros)
